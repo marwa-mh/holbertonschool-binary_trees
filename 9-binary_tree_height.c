810bf11e3280d9a5a5298ae2a0bbc11f0e8b2cfd
Marwa <marwa.almahmoud@hotmail.com>
2025-04-08 02:15:47 +0000

    Add 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,24 +2,29 @@
 /**
  * binary_tree_height - postorder the binary tree
  * @tree: pointer to the root
+ * @func: pointer to a function
  * Return: Nothing
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height = 0;
+	size_t left_height = 0, right_height = 0, max_height = 0;
 
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
-		return (height);
-
+		return (max_height);
 	if (tree->left != NULL)
 	{
-		height = 1 + binary_tree_height(tree->left);
+		left_height = 1 + binary_tree_height(tree->left);
 	}
 	if (tree->right != NULL)
 	{
-		height = 1 + binary_tree_height(tree->right);
+		right_height = 1 + binary_tree_height(tree->right);
 	}
-	return (height);
+	if (left_height > right_height)
+		max_height = left_height;
+	else
+		max_height = right_height;
+
+	return (max_height);
 }
