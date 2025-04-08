81782385b130b8b00a45b16176ca31ab71013c4c
Marwa <marwa.almahmoud@hotmail.com>
2025-04-08 20:52:12 +0000

    fix 14-balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,10 +1,9 @@
-#include "binary_trees.h"
 /**
- * binary_tree_height - return the height for the binary tree
+ * binary_tree_balance - return the balance for the binary tree
  * @tree: pointer to the root
  * Return: the size
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+int binary_tree_balance(const binary_tree_t *tree)
 {
 	size_t left_height = 0, right_height = 0, max_height = 0;
 
@@ -16,24 +15,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		left_height = 1 + binary_tree_height(tree->left);
 	if (tree->right != NULL)
 		right_height = 1 + binary_tree_height(tree->right);
-	if (left_height > right_height)
-		max_height = left_height;
-	else
-		max_height = right_height;
-	return (max_height);
-}
-/**
- * binary_tree_balance - return the balance for the binary tree
- * @tree: pointer to the root
- * Return: the size
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	size_t left_height = 0, right_height = 0;
-
-	if (tree == NULL)
-		return (0);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
 	return (left_height - right_height);
 }
