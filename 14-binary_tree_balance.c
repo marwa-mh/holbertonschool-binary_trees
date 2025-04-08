d5e55b78239861de2e0c156f2a354b6944e6ff35
Marwa <marwa.almahmoud@hotmail.com>
2025-04-08 05:13:01 +0000

    Add 14-balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,8 +11,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->left != NULL)
-		left_height = 1 + binary_tree_height(tree->left);
+		left_height = 1 + binary_tree_balance(tree->left);
 	if (tree->right != NULL)
-		right_height = 1 + binary_tree_height(tree->right);
+		right_height = 1 + binary_tree_balance(tree->right);
 	return (left_height - right_height);
 }
